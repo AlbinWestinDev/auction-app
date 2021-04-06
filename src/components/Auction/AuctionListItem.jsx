25e3abb52458cadfcf0998d866b3f12f1408d2aa
Malin <a.malin.roos@gmail.com>
2021-04-06 23:39:22 +0200

    Datumformattering med Day.js och klickbara kort

--- src/components/Auction/AuctionListItem.jsx
+++ src/components/Auction/AuctionListItem.jsx
@@ -2,55 +2,36 @@
 import React from 'react';
 import './AuctionListStyle.css';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 const AuctionItem = ({ auction }) => {
-  // const startDate = () => {
-  //     var d = new Date(auction.StartDatum),
-  //     month = '' + (d.getMonth() + 1),
-  //     day = '' + d.getDay(),
-  //     year = '' + d.getFullYear();
 
-  //     if (month.length < 2)
-  //         month = '0' + month;
-  //     if (day.length < 2)
-  //         day = '0' + day;
-
-  //     return [year, month, day].join('-');
-  // };
-
-  // const endDate = () => {
-  //   var d = new Date(auction.SlutDatum),
-  //     month = '' + (d.getMonth() + 1),
-  //     day = '' + d.getDay(),
-  //     year = '' + d.getFullYear();
-
-  //   if (month.length < 2) month = '0' + month;
-  //   if (day.length < 2) day = '0' + day;
-
-  //   return [year, month, day].join('-');
-  // };
+  let d = dayjs(auction.SlutDatum);
+  console.log(d.format("DD-MM-YYYY"));
 
   return (
+  
+  <Link style={{textDecoration:'none', color:'black'}}
+    to={{
+      pathname: `/Auktion/2310/${auction.AuktionID}`,
+      auctionObj: auction
+    }}>
+
     <div className="a-comp-holder">
       <div className="a-comp-card">
         <div className="a-card-container">
           <h4>{auction.Titel}</h4>
 
           <p>Utropspris: {auction.Utropspris} kr</p>
+          
+          <p>Slutdatum: <br/>
+          {d.format("DD-MM-YYYY")} <br/>
+          {d.format("HH:ss")}</p>
 
-          <p>Slutdatum: {auction.SlutDatum}</p>
-
-          <Link
-            to={{
-              pathname: `/Auktion/2310/${auction.AuktionID}`,
-              auctionObj: auction,
-            }}
-          >
-            Se Mer
-          </Link>
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
