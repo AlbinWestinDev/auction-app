e46c300221cec654378e966bebd42fba9bb0f41b
Viktor Karlström <46776740+ViktorKarlstrom@users.noreply.github.com>
2021-04-07 14:25:37 +0200

    search functionality

--- src/components/Auction/Search/SearchAuction.jsx
+++ src/components/Auction/Search/SearchAuction.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import { getAll } from '../../../DataAPIManagerTool/NackowskisService';
+import '../AuctionListStyle.css';
+import AuctionListItem from '../AuctionListItem';
 
 const SearchAuction = () => {
-    
-    let { searchTerm } = useParams();
+  let { searchTerm } = useParams();
+  const [auctions, setAuctions] = useState();
 
-    console.log(searchTerm);
+  const getAuctions = async () => {
+    const result = await getAll();
 
-    return (
-        <>
-        </>
-        )
-}
- 
+    setAuctions(
+      result.filter((auc) =>
+        auc.Titel.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  };
+
+  useEffect(() => {
+    getAuctions();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="a-list-grid">
+          {auctions?.map((auction) => (
+            <AuctionListItem key={auction.AuktionID} auction={auction} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default SearchAuction;
